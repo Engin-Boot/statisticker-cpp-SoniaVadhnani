e82fb15bccfd01170c849a34fc70c819a1fbc8fc
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-09-10 19:34:59 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
+#include<cmath>
+#include<algorithm> 
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats::Stats() {}
+Statistics::Stats::Stats(float avg, float min, float max) {
+    this->average = avg;
+    this->max = max;
+    this->min = min;
+}
+
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
+
+    if (numbers.size() == 0) {
+        return Stats(NAN, NAN, NAN);
+    }
+    float min = *min_element(numbers.begin(), numbers.end());
+    float max = *max_element(numbers.begin(), numbers.end());
+    float sum = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+        sum += numbers[i];
+    }
+    float avg = sum / numbers.size();
+    return Statistics::Stats(avg, min, max);
 }
